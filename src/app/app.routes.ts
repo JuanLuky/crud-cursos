92707b073acbed87be5cfd112c5ddcbdd86454f4
Juan Carlos <juandev33@gmail.com>
2024-03-21 23:27:16 -0300

    feet: "Atualizamos o site colocando os botões de ações, criamos um componente de formulario onde vamos criar os cursos, criamos também uma rota para adicionar um novo curso,"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,17 @@
 import { Routes } from '@angular/router';
 import { CoursesComponent } from './components/courses/courses.component';
+import { FormsComponent } from './components/forms/forms.component';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'courses'},
+  { path: '', component: CoursesComponent},
   { path: 'courses', component: CoursesComponent},
+  { path: 'courses/new', component: FormsComponent},
 
+  { path: '**', pathMatch: 'full', redirectTo: '/courses'},
 
   {
     path: 'courses',
     loadChildren: () =>
       import('./components/courses/courses.component').then(c => c.CoursesComponent)
-  }
+  },
 ];
--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -12,21 +12,26 @@ import { CoursesService } from '../../services/courses.service';
 import { AsyncPipe } from '@angular/common';
 import { ErrorComponent } from '../../shared/error/error.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [ MatTableModule, MatCardModule, MatInputModule, MatFormFieldModule, FormsModule, MatToolbarModule,MatProgressSpinnerModule, AsyncPipe, ErrorComponent],
+  imports: [ MatTableModule, MatCardModule, MatInputModule, MatFormFieldModule, FormsModule, MatToolbarModule,MatProgressSpinnerModule, AsyncPipe, ErrorComponent, MatButtonModule, MatIconModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
   courses$: Observable<Course[]>;
-  displayedColumns = ['name', 'category', 'language']
+  displayedColumns = ['name', 'category', 'language', 'actions']
 
   private coursesService = inject(CoursesService);
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
 
-  constructor( public dialog: MatDialog ) {
+  constructor( public dialog: MatDialog) {
     this.courses$ = this.coursesService.list()
     .pipe(
       catchError(error => {
@@ -34,6 +39,10 @@ export class CoursesComponent {
         return of([]);
       })
     );
+  }
+
+  onAdd() {
+    this.router.navigate(['new'], {relativeTo: this.route});
   }
 
   onError( errorMsg: string) {
